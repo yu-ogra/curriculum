7fc498bd852cdb0ce93920093469f3beba0c188e
yuu-ogura <143790511+yu-ogra@users.noreply.github.com>
2024-03-05 22:30:33 +0900

    Add files via upload

---
+++ study/2-21/Task2_21.java
@@ -0,0 +1,61 @@
+package study;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+/**
+ * 本課題では、代表的なコレクション・フレームワークのArrayList及びHashMapを使い
+ * くだものとその値段を管理するコードを記述します。
+ * 
+ * 問①～問④まであります。
+ * 
+ */
+public class Task2_21 {
+    
+    public static final String orange = "オレンジ";
+    public static final String grape = "ぶどう";
+    public static final String apple = "りんご";
+    public static final String pear = "梨";
+
+    public static void main(String[] args) {
+        // ① 変数名fruitsArrayListのデータ型StringでArrayListを宣言し、
+        // 変数orange、grape、apple、pearの値を順に格納して下さい。
+    	ArrayList<String> fruits = new ArrayList<String>();
+    	
+    	fruits.add(orange);
+    	fruits.add(grape);
+    	fruits.add(apple);
+    	fruits.add(pear);
+
+        // ② 変数名fruitsHashMapのHashMapを宣言（データ型　キーはString、値はInteger）し、
+        // キーにはfruitsArrayListに格納した値を格納し、
+        // 値にはオレンジは150円、ぶどうは120円、りんごは180円、梨は140円を格納して下さい。
+        // 値は数値のみで単位の円は入れないでください。
+
+    	HashMap<String, Integer> fruitshashMap = new HashMap<String, Integer>();
+
+        fruitshashMap.put(orange, 150);
+        fruitshashMap.put(grape, 120);
+        fruitshashMap.put(apple, 180);
+        fruitshashMap.put(pear, 140);
+        // ③ オレンジの値段を200円に上書きしてください。
+
+        fruitshashMap.replace(orange,200);
+        
+        
+        // ④ レッスン内容を参考に拡張for文を使い、
+        // 実行結果がターミナルと同じように表示されるよう出力して下さい。
+		
+      
+          String[] arrayList = {apple, grape, pear, orange};
+          int[] number = {180, 120, 140, 200};
+            
+          for (String name : arrayList) for (int price : number)  {
+           System.out.println(name + "の値段は" + price + "です。");
+            }
+        }
+    }
+          
+		
+
+
+    
