165225c296a82a41271cd7d7db3e771542656858
yuu-ogura <143790511+yu-ogra@users.noreply.github.com>
2024-03-17 17:59:57 +0900

    Add files via upload

---
+++ study/2-23/Task2_23.java
@@ -0,0 +1,57 @@
+package study;
+
+import java.util.ArrayList;
+
+import constants.Constants;
+import language.Student;
+import person.Person;
+        
+        
+public class Task2_23 {
+
+    public static void main(String[] args) {
+        //Personクラスのインスタンスを格納するArrayListクラス型の変数persons
+        ArrayList<Person> persons = new ArrayList<Person>(); 
+        
+        //①Personクラスの変数名「yamada」というインスタンスを作成して下さい。
+        //引数には1:山田太郎 2:Java を入れて下さい。
+        //また「Java」は、Constants.javaのものを扱って下さい。
+
+        //ここに解答
+        Person yamada = new Person("山田太郎","LANGUAGE_JAVA");
+    	}
+    	
+    
+    	public void Constants(){
+    	String LANGUAGE_JAVA = "Java";
+    	}
+    	
+
+        
+        //②作成した変数「yamada」を利用し名前を表示して下さい。
+
+        //ここに解答
+    	public void status() {
+    		char[] name = null;
+			System.out.println(name);
+    	}
+        
+        //③Personクラスの変数名「ishitani」というインスタンスを作成して下さい。
+        //引数には1:石谷花子 2:HTML を入れて下さい。
+        //また「HTML」は、Constants.javaのものを扱って下さい。
+        
+        //ここに解答
+        
+        
+        //④作成した変数「ishitani」を利用し学んでいる言語を表示して下さい。
+
+        //ここに解答
+        
+        //⑤「〇〇が△△を学んでいます」という表示で山田さん、石谷さんが何を学んでいるか出力して下さい。
+        
+        //ここに解答
+
+	}
+
+
+    
